0f4bc5f2d7302c855df094352ce1b3202dcb29e5
avrip <avripsec@protonmail.com>
2022-08-26 11:42:54 +0200

    this don't work, but I backup it

--- main.go
+++ main.go
@@ -12,21 +12,38 @@ import (
 const numberofguesses int = 3
 
 var gamename = "Cinemojie"
+var answer = "catchmeifyoucan"
 
 func main() {
 
 	greetUsers()
 	fmt.Println("First propostion:")
-	emoji.Println(":monkey::earth_americas:")
-	for {
-		userGuess := getUserInput()
-		answer := "planetoftheapes"
-		if userGuess == answer {
-			fmt.Println("Congratulation!!")
-			break
-		} else {
-			fmt.Println("nope, try again.")
-		}
+	propostionOne()
+	userGuess := getUserInput()
+	userGuess2 := getUserInput2()
+	userGuess3 := getUserInput3()
+
+	if userGuess == answer {
+		fmt.Println("Congratulation! You win 3pts")
+	} else if userGuess != answer {
+		fmt.Println("nope, try again.")
+		propostionTwo()
+		getUserInput2()
+	}
+
+	if userGuess2 == answer {
+		fmt.Println("Nice, you win 2pts")
+
+	} else if userGuess2 != answer {
+		fmt.Println("Nope again... last try")
+		propostionThree()
+		getUserInput3()
+	}
+	if userGuess3 == answer {
+		fmt.Println("Finally you get it! You win 1pt")
+
+	} else if userGuess3 != answer {
+		fmt.Printf("No.. that's bad, the answer was %v!\n", answer)
 
 	}
 }
@@ -42,4 +59,30 @@ func getUserInput() string {
 	fmt.Println("Enter your guess:")
 	fmt.Scan(&userGuess)
 	return userGuess
+}
+
+func getUserInput2() string {
+	var userGuess2 string
+	fmt.Println("Enter your second guess:")
+	fmt.Scan(&userGuess2)
+	return userGuess2
+}
+
+func getUserInput3() string {
+	var userGuess3 string
+	fmt.Println("Enter your last guess:")
+	fmt.Scan(&userGuess3)
+	return userGuess3
+}
+
+func propostionOne() {
+	emoji.Println(":airplane:")
+}
+
+func propostionTwo() {
+	emoji.Println(":airplane::man_running:")
+}
+
+func propostionThree() {
+	emoji.Println(":airplane::man_running::money_with_wings:")
 }
